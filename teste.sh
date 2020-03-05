034bd6fb1c77baf62f0578ea2007ec2168b21635
aluno <aluno@Lab-03>
2020-03-05 10:44:15 -0300

    a

--- teste.sh
+++ teste.sh
@@ -5,12 +5,15 @@
 # Ana Beatriz
 
 cd imagens-livros
+echo 'convertendo as imagens' 
 for imagem in *.jpg
 do 
         echo $imagem 
         img_sem_ext=$(ls $imagem | awk -F. '{print $1}')
+	echo ' imagens convertidas'
         echo $img_sem_ext 
         convert $imagem $img_sem_ext.png
+	echo ' imagens convertidas, finish'
 done
 
 cd ..
