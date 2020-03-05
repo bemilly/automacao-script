2bb0c892cd936d67af718272abf5c3180298a0d7
aluno <aluno@Lab-03>
2020-03-05 10:23:11 -0300

    aula 05/03

---
+++ teste.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# script de converter imagens
+#
+# Ana Beatriz
+
+cd imagens-livros
+for imagem in *.jpg
+do 
+        echo $imagem 
+        img_sem_ext=$(ls $imagem | awk -F. '{print $1}')
+        echo $img_sem_ext 
+        convert $imagem $img_sem_ext.png
+done
+
+cd ..
+
+
